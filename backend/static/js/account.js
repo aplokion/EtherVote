ea2610000055bec3e58ff1b32ef8fab948d8b419
7Maersk <leskovm02@gmail.com>
2024-01-19 20:21:10 +0800

    remade homepage and something else.....

--- backend/static/js/account.js
+++ backend/static/js/account.js
@@ -1,22 +1,69 @@
-$(document).ready(function () {
-    $.get('/account/get_user_elections_list', function (data) {
-        const electionsList = $('#elections-list');
+document.addEventListener('DOMContentLoaded', function () {
+    const electionsList = document.getElementById('elections-list');
+    const electionsHeading = document.getElementById('elections-heading');
+    const participationButton = document.getElementById('participationButton');
+    const createdButton = document.getElementById('createdButton');
 
-        if ($.isEmptyObject(data)) {
-            electionsList.append('<li class="list-group-item">Пользователь не учавствовал в голосованиях</li>');
-        } else {
-            $.each(data, function (electionId, electionInfo) {
-                electionsList.append(
-                    '<li class="list-group-item">' +
-                    '<strong>Голосование №' + electionId + '</strong><br>' +
-                    'Тема: ' + electionInfo.topic + '<br>' +
-                    'Дата начала: ' + electionInfo.date_start + '<br>' +
-                    'Дата окончания: ' + electionInfo.date_end + '<br>' +
-                    'Создатель: ' + electionInfo.creator + '<br>' +
-                    'Выбор: ' + electionInfo.choice +
-                    '</li>'
-                );
-            });
-        }
+    participationButton.addEventListener('click', function () {
+        fetchElectionsList('/account/get_user_elections_list', 'Голосования, в которых вы участвовали:');
     });
+
+    createdButton.addEventListener('click', function () {
+        fetchElectionsList('/account/get_user_created_elections_list', 'Созданные вами голосования:');
+    });
+
+    fetchElectionsList('/account/get_user_elections_list', 'Голосования, в которых вы участвовали:');
+
+    function fetchElectionsList(url, headingText) {
+        const xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    const data = JSON.parse(xhr.responseText);
+
+                    if (Object.keys(data).length === 0) {
+                        electionsHeading.innerText = 'Нет доступных голосований';
+                        electionsList.innerHTML = '<li class="list-group-item">Нет доступных голосований</li>';
+                    } else {
+                        electionsHeading.innerText = headingText;
+                        electionsList.innerHTML = '';
+                        Object.entries(data).forEach(([electionId, electionData]) => {
+                            const votingElement = createVotingElement(electionData);
+                            electionsList.appendChild(votingElement);
+                        });
+                    }
+                } else {
+                    console.error('Error:', xhr.status, xhr.statusText);
+                }
+            }
+        };
+
+        xhr.open('GET', url, true);
+        xhr.send();
+    }
+
+    function createVotingElement(electionData) {
+        const votingElement = document.createElement('li');
+        votingElement.className = 'list-group-item';
+        votingElement.innerHTML = `
+            <strong>Тема: ${electionData.topic}</strong><br>
+            Описание: ${electionData.description}<br>
+            ${createOptionsList(electionData.option_indices, electionData.votes)}
+        `;
+        return votingElement;
+    }
+
+    function createOptionsList(options, votes) {
+        let optionsList = '<ul>';
+        for (let i = 0; i < options.length; i++) {
+            const percentage = calculatePercentage(votes[i], votes.reduce((a, b) => a + b, 0));
+            optionsList += `<li>${options[i]}: ${votes[i]} (${percentage.toFixed(2)}%)</li>`;
+        }
+        optionsList += '</ul>';
+        return optionsList;
+    }
+
+    function calculatePercentage(value, total) {
+        return total === 0 ? 0 : (value / total) * 100;
+    }
 });
